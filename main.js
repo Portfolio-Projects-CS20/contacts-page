aa511609a68a271b70cf8a0c27f96314644b6b13
Navika-M <47578845+Navika-M@users.noreply.github.com>
2020-07-20 12:32:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 // My Contacts Page
 
+// Global Variables
+let divEl;
+let copyInfo;
+
+// Event Listeners
+document.getElementById("showContacts").addEventListener("click", showAllContacts);
+
+// Event Functions
+function showAllContacts() {
+    for (let i = 0; i < contactNames.length; i++) {
+        // Create new div and copy name:
+        divEl = document.getElementById("contact-person");
+        copyInfo = divEl.cloneNode(true);
+        document.body.append(copyInfo);
+
+
+        displayName("name", "Name", contactNames[i]);
+        displayName("phoneNumber", "Phone Number", phoneNumbers[i]);
+        displayName("birthday", "Birthday", birthDay[i]);
+    }
+
+}
+
+
+function displayName(id, section, variable) {
+    document.getElementById(id).innerHTML = section + ": " + variable;
+}
+
 // Load names.txt file
 let contactNames;
 fetch("names.txt")
@@ -17,27 +45,3 @@ let birthDay;
 fetch("birthday.txt")
     .then((rawDates) => rawDates.text())
     .then((dates) => birthDay = dates.split("\r\n"));
-
-
-// Event Listeners
-document.getElementById("showContacts").addEventListener("click", showName);
-
-// Event Functions
-function showName() {
-
-    for (let i = 0; i < contactNames.length; i++) {
-        // Create new div and copy name:
-        let divEl = document.getElementById("contact-person");
-        let copyInfo = divEl.cloneNode(true);
-        document.body.appendChild(copyInfo);
-
-        displayName("name", "Name", contactNames[i]);
-        displayName("phoneNumber", "Phone Number", phoneNumbers[i]);
-        displayName("birthday", "Birthday", birthDay[i]);
-    }
-}
-
-
-function displayName(id, section, variable) {
-    document.getElementById(id).innerHTML = section + ": " + variable;
-}
