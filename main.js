c2c0918b90c72645d169ea6d81e287678e4e1d2d
Navika-M <47578845+Navika-M@users.noreply.github.com>
2020-07-20 09:53:40 -0600

    show all contacts

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 // My Contacts Page
 
+// Load names.txt file
+let contactNames;
+fetch("names.txt")
+    .then((rawNames) => rawNames.text())
+    .then((names) => contactNames = names.split("\r\n"));
+
+// Load phone.txt file
+let phoneNumbers;
+fetch("phone.txt")
+    .then((rawPhone) => rawPhone.text())
+    .then((phoneNum) => phoneNumbers = phoneNum.split("\r\n"));
+
+// Load birthday.txt file
+let birthDay;
+fetch("birthday.txt")
+    .then((rawDates) => rawDates.text())
+    .then((dates) => birthDay = dates.split("\r\n"));
+
+
 // Event Listeners
-document.getElementById("showContacts").addEventListener("click", showContacts);
+document.getElementById("showContacts").addEventListener("click", showName);
 
 // Event Functions
-function showContacts(){
-    displayName("Navika", "565-565-434", "June 45");
-    
+function showName() {
+
+    for (let i = 0; i < contactNames.length; i++) {
+        // Create new div and copy name:
+        let divEl = document.getElementById("contact-person");
+        let copyInfo = divEl.cloneNode(true);
+        document.body.appendChild(copyInfo);
+
+        displayName("name", "Name", contactNames[i]);
+        displayName("phoneNumber", "Phone Number", phoneNumbers[i]);
+        displayName("birthday", "Birthday", birthDay[i]);
+    }
 }
 
-function displayName(name, phoneNumber, birthday){
-    document.getElementById("name").innerHTML = "Name: " + name;
-    document.getElementById("phone-number").innerHTML = "Phone Number: " + phoneNumber;
-    document.getElementById("birthday").innerHTML = "Birthday: " + birthday;
+
+function displayName(id, section, variable) {
+    document.getElementById(id).innerHTML = section + ": " + variable;
 }
